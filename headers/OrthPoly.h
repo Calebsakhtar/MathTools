450e6de512d30a10a2a91a63b2d29d85b250be0e
Caleb AM <calebsakhtar@gmail.com>
2023-04-05 23:22:51 +0100

    Implement the Jacobi polynomials

--- headers/OrthPoly.h
+++ headers/OrthPoly.h
@@ -3,27 +3,28 @@
 
 #include <cmath>
 #include <vector>
+#include "MathTools.h"
 
 namespace MathTools {
 
 	class OrthPoly {
 	protected:
-		unsigned int m_n; // Polynomial order
+		size_t m_n; // Polynomial order
 
 	public:
 		OrthPoly() {};
 
-		OrthPoly(const unsigned int n) {
+		OrthPoly(const size_t n) {
 			m_n = n;
 		};
 
 		double evaluate(const double x) const;
 
-		unsigned int get_order() const{
+		size_t get_order() const{
 			return m_n;
 		}
 
-		void set_order(const unsigned int n){
+		void set_order(const size_t n){
 			m_n = n;
 		};
 		
@@ -35,7 +36,7 @@ namespace MathTools {
 		using OrthPoly::OrthPoly;
 	public:
 		virtual double evaluate(const double x) const{
-			// See documentation: https://en.cppreference.com/w/cpp/numeric/special_functions/legendre
+			// See documentation: https://en.cppreference.com/w/cpp/numeric/special_functions/legendre			
 			return std::legendre(m_n, x);
 		}
 	};
@@ -60,6 +61,45 @@ namespace MathTools {
 			// See documentation: https://en.cppreference.com/w/cpp/numeric/special_functions/laguerre
 			return std::laguerre(m_n, x);
 		}
+	};
+
+	// *********** JACOBI POLYNOMIALS *********** //
+	class JacobiPoly : public OrthPoly {
+		double m_alpha = 0; // parameter alpha
+		double m_beta = 0; // parameter beta
+		std::vector<double> m_coeffs = {};
+
+	public:
+		JacobiPoly() {};
+
+		// Use the summation expression for the Jacobi polynomials
+		// to store the relevant coefficients.
+		//
+		// See: https://en.wikipedia.org/wiki/Jacobi_polynomials#Alternate_expression_for_real_argument
+		JacobiPoly(const size_t n, const double alpha, const double beta);
+
+		// Evaluate the summation form of the Jacobi polynomial 
+		// using the stored coefficient.
+		//
+		// See: https://en.wikipedia.org/wiki/Jacobi_polynomials#Alternate_expression_for_real_argument
+		virtual double evaluate(const double x) const;
+
+		double get_alpha() const {
+			return m_alpha;
+		}
+
+		void set_alpha(const double alpha) {
+			m_alpha = alpha;
+		};
+
+		double get_beta() const {
+			return m_beta;
+		}
+
+		void set_beta(const double beta) {
+			m_alpha = beta;
+		};
+
 	};
 }
 
--- src/OrthPoly.cpp
+++ src/OrthPoly.cpp
@@ -1,3 +1,48 @@
 #include "../headers/OrthPoly.h"
 
+namespace MathTools {
 
+	JacobiPoly::JacobiPoly(const size_t n, const double alpha,
+		// Use the summation expression for the Jacobi polynomials
+		// to store the relevant coefficients.
+		//
+		// See: https://en.wikipedia.org/wiki/Jacobi_polynomials#Alternate_expression_for_real_argument
+
+		const double beta) {
+		m_n = n;
+		m_alpha = alpha;
+		m_beta = beta;
+ 
+		double coeff = 0;
+		double s_double = 0;
+		const double n_double = static_cast<double>(n);
+
+		// Loop from 0 to n, both inclusive
+		for (size_t s = 0; s < n + 1; s++){
+			s_double = static_cast<double>(s);
+			coeff = nChoosek_gamma(n_double + alpha, n_double - s_double);
+			coeff /= pow(2, n_double);
+			coeff *= nChoosek_gamma(n_double + beta, s_double);
+			m_coeffs.push_back(coeff);
+		}
+	}
+
+	double JacobiPoly::evaluate(const double x) const {
+		// Evaluate the summation form of the Jacobi polynomial 
+		// using the stored coefficient.
+		//
+		// See: https://en.wikipedia.org/wiki/Jacobi_polynomials#Alternate_expression_for_real_argument
+
+		double result = 0;
+		double s_double = 0;
+		const double n_double = static_cast<double>(m_n);
+
+		for (size_t s = 0; s < m_coeffs.size(); s++) {
+			s_double = static_cast<double>(s);
+			result += m_coeffs[s] * pow(x - 1, s_double)
+				* pow(x + 1, n_double - s_double);
+		}
+
+		return result;
+	}
+}
