29ccb41d47ee91f2b0ca9838f0ea0fc283a652ce
Caleb AM <calebsakhtar@gmail.com>
2023-04-11 20:08:15 +0100

    Remove circular includes

--- headers/OrthPoly.h
+++ headers/OrthPoly.h
@@ -3,7 +3,6 @@
 
 #include <cmath>
 #include <vector>
-#include "MathTools.h"
 
 namespace MathTools {
 
--- src/OrthPoly.cpp
+++ src/OrthPoly.cpp
@@ -1,4 +1,5 @@
 #include "../headers/OrthPoly.h"
+#include "../headers/MathTools.h"
 
 namespace MathTools {
 
